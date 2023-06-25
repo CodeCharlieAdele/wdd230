b879bbb916676e2e0603883b8f9e795cd67e756c
CodeCharlieAdele <charlotteadeletn@gmail.com>
2023-06-24 21:26:55 -0400

    still working on it

--- chamber/scripts/visitCounter.js
+++ chamber/scripts/visitCounter.js
@@ -30,3 +30,29 @@ function displayDaysSinceLastVisit() {
 
   visitsDisplay.innerText = 'number of days since last visit: ' + differenceInDays;
 }
+
+
+// 1️⃣ Initialize display element variable
+const visitsDisplay = document.querySelector(".visits");
+let diffDay;
+if (currentDate !== lastVisitDate) {
+	diffDay = true;
+}
+
+// 2️⃣ Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+// 3️⃣ Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
+if (numVisits == 1) {
+	visitsDisplay.textContent = `Welcome! It's your first time.`;
+} if (numVisits > 1 && diffDay == false) {
+	visitsDisplay.textContent = `Back so soon!`;
+} if (diffDay == true) {
+	visitsDisplay.textContent = `You visited ` + differenceInDays + ` days ago`;
+}
+
+// 4️⃣ increment the number of visits by one.
+numVisits++;
+
+// 5️⃣ store the new visit total into localStorage, key=numVisits-ls
+localStorage.setItem("numVisits-ls", numVisits);
